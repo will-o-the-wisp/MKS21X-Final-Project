94e990c2aadade0b2cc8bbf768e1141e271ef188
will-o-the-wisp <kli18@stuy.edu>
2019-01-04 10:38:43 -0500

    added skeleton for Room

---
+++ Room.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public static class Room{
+  private int botLeftCornerX;
+  private int botLeftCornerY;
+  private int width;
+  private int height;
+  Room(int depth){
+
+  }
+}
