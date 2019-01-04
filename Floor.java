09336ddb1486359553fe3e8f083eb47132d26bcd
will-o-the-wisp <kli18@stuy.edu>
2019-01-04 10:36:09 -0500

    added skeleton for Floor

---
+++ Floor.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public static class Floor{
+  //private List<Room> rooms;
+  //private List<Path> paths;
+  private int entranceX;
+  private int entranceY;
+  private int exitX;
+  private int exitY;
+  Floor(int depth){
+    
+  }
+}
