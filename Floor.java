b9a6933d6fb197e4eb7a61b09f35775fc39476bf
will-o-the-wisp <kli18@stuy.edu>
2019-01-07 10:55:48 -0500

    added testing constructor to Floor

--- Floor.java
+++ Floor.java
@@ -1,12 +1,20 @@
 import java.util.List;
 
 public class Floor{
-  //private List<Room> rooms;
+  private List<Room> rooms;
   //private List<Path> paths;
   private int entranceX;
   private int entranceY;
   private int exitX;
   private int exitY;
+  //private char[][] grid;
+  Floor(List<Room> _rooms, int enX, int enY, int exX, int exY){
+    rooms = _rooms;
+    entranceX = enX;
+    entranceY = enY;
+    exitX = exX;
+    exitY = exY;
+  }
   Floor(int depth){
 
   }
