39c3f25353fea1c333eb6f92af73f955e58e56ac
Brandon Chao <bchao1@stuy.edu>
2019-01-08 00:22:16 -0500

    constructors and get/set for item entity

--- Item.java
+++ Item.java
@@ -2,5 +2,29 @@ public class Item extends Interactive{
 
   private boolean onTheGround;
   private String type;
-  
+
+  public Item(){
+    onTheGround = true;
+    type = "";
+  }
+
+  public Item(boolean status, String use) {
+    onTheGround = status;
+    type = use;
+  }
+  public String getType(){
+    return type;
+  }
+
+  public void setType(String newTypes){
+    type = newTypes;
+  }
+
+  public boolean getStatus(){
+    return onTheGround;
+  }
+
+  public void setStatus(boolean status){
+    onTheGround = status;
+  }
 }
