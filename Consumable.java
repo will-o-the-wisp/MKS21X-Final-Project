9845cd67491832685de481401fdc14ee1f1ef16f
Brandon Chao <bchao1@stuy.edu>
2019-01-08 00:38:48 -0500

    constructor + eget/set

--- Consumable.java
+++ Consumable.java
@@ -1,4 +1,16 @@
 public class Consumable extends Item{
 
   private int duration;
+
+  public Consumable(){
+    duration = 0;
+  }
+
+  public void setDuration(int time){
+    duration = time;
+  }
+
+  public int getDuration(){
+    return duration;
+  }
 }
