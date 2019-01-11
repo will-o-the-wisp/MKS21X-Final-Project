8a38fc60059e88f1236340c90e8da8d2a6d41778
Brandon Chao <bchao1@stuy.edu>
2019-01-11 00:16:18 -0500

    random equipments will be generated from a txt file which can be added onto, also set/get for the modifiable constants

--- Equipment.java
+++ Equipment.java
@@ -1,6 +1,9 @@
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Equipment extends Item{
 
@@ -9,15 +12,42 @@ public class Equipment extends Item{
   private List<String> types = new ArrayList<String>();  //need a dictionary for this?
   private Random seed;
 
-  public Equipment(){
-    durability = 0;
-    name = null;
+  public Equipment() throws FileNotFoundException{
+    Scanner into = new Scanner(new File("equips.txt"));
+    while (into.hasNext()) {
+      types.add(into.nextLine());
+    }
     seed = new Random();
+    durability = seed.nextInt(100);
+    name = getRandomDropName();
   }
 
-  public String getRandomDrop(){
-    return types.get(seed.nextInt() % types.size());
+  public String getRandomDropName(){
+    return types.get(seed.nextInt(types.size()));
   }
 
+  public List<Integer> getRandomStats(){
+    List<Integer> staters = new ArrayList<Integer>();
+    staters.add(seed.nextInt(20));
+    staters.add(seed.nextInt(10));
+    staters.add(seed.nextInt(10));
+    return staters;
+  }
+
+  public int getDurability(){
+    return durability;
+  }
+
+  public void setDurability(int uses){
+    durability = uses;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public void setName(String title){
+    name = title;
+  }
 
 }
