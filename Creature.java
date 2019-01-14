a6be4f1a202bd079c3eb902f22fa8e69d4bcb75f
Brandon Chao <bchao1@stuy.edu>
2019-01-13 23:52:57 -0500

    will have to add check for wall when project comes together

--- Creature.java
+++ Creature.java
@@ -22,22 +22,22 @@ public class Creature extends Interactive{
     direction = dire;
   }
 
-/*
-  public Entity lookInFront(){
+
+  public Entity lookInFront(Floor f){
     if (direction.equals("North")) {
-      return getGrid[getY()+1][getX()]; //direction might not be correct these are based on normal x y axis
+      return f.getGrid[getY()+1][getX()]; //direction might not be correct these are based on normal x y axis
     }
     else if (direction.equals("East")) {
-      return getGrid[getY()][getX()+1]; //direction might not be correct
+      return f.getGrid[getY()][getX()+1]; //direction might not be correct
     }
     else if (direction.equals("South")) {
-      return getGrid[getY()-1][getX()]; //direction might not be correct
+      return f.getGrid[getY()-1][getX()]; //direction might not be correct
     }
     else if (direction.equals("West")) {
-      return getGrid[getY()][getX()-1]; //direction might not be correct
+      return f.getGrid[getY()][getX()-1]; //direction might not be correct
     }
   }
-*/
+
   public boolean meleeAttack(Creature defender){
     defender.setHP(defender.getHP() - attackCalc(defender));
     this.setHP(this.getHP() - defendCalc(defender));
