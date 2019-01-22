2b69cec7eb633b2f236785b34323b1824183ba35
Brandon Chao <bchao1@stuy.edu>
2019-01-21 20:33:07 -0500

    spacing for floor

--- Benelux.java
+++ Benelux.java
@@ -51,7 +51,7 @@ public class Benelux{
 		String row = "";
 		for (int i = 0; i<fr.getGrid().length-1;i++){
 			for (int o =0; o<fr.getGrid()[i].length-1;o++){
-				row += fr.getGrid()[i][o];
+				row += " " + fr.getGrid()[i][o] + " ";
 				if (o == fr.getGrid()[i].length -2){
 					row += "\n";
 				}
