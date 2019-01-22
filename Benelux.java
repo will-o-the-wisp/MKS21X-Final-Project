3dad75439e0246e8eae1ff3ce210cf4b3a48de4f
Brandon Chao <bchao1@stuy.edu>
2019-01-21 21:23:20 -0500

    see which row is outlier

--- Benelux.java
+++ Benelux.java
@@ -54,8 +54,8 @@ public class Benelux{
 		for (int i = 0; i<fr.getGrid().length-1;i++){
 			for (int o =0; o<fr.getGrid()[i].length-1;o++){
 				row += " " + fr.getGrid()[i][o] + " ";
-				if (o == fr.getGrid()[i].length -1){
-					row += "\n";
+				if (o == fr.getGrid()[i].length -2){
+					row += i + "\n";
 				}
 			}
 		}
